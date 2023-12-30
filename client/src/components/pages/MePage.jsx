b59a163c99faae8d122a326ede9c9b9ce09717bc
ChgDave <chgdave@gmail.com>
2023-12-30 17:00:46 -0600

    added query in the me page to query user data

--- client/src/components/pages/MePage.jsx
+++ client/src/components/pages/MePage.jsx
@@ -1,3 +1,16 @@
+import { useQuery } from "@apollo/client";
+import { QUERY_ME } from "../../utils/queries";
+
 export default function MePage() {
-  return <div>MePage</div>;
+  const { loading, data } = useQuery(QUERY_ME);
+  const user = data.me;
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      MePage
+      <div>Welcome back! {user.name}</div>
+    </div>
+  );
 }
