87d0395b8362707b70f66e5655e2addff24651d1
justmann <mj.justmann@gmail.com>
2024-01-01 16:21:05 -0600

    possible avatar upload

--- client/src/components/LibraryCard/LibraryCard.jsx
+++ client/src/components/LibraryCard/LibraryCard.jsx
@@ -1,6 +1,22 @@
+import React, { useState } from "react";
+import AvatarEditor from "react-avatar-editor";
 import "./LibraryCard.scss";
+const avatar = "../../images/InitialAvatar.jpg";
 
 const LibraryCard = ({ user }) => {
+  const [uploadedImage, setUploadedImage] = useState(avatar);
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setUploadedImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="main-card">
       <div className="top-row">
@@ -8,7 +24,29 @@ const LibraryCard = ({ user }) => {
         <h1>{user.name}</h1>
       </div>
       <div className="user-profile">
-        <img src="#" alt="avatar" />
+        <div className="image-upload-container">
+          <AvatarEditor
+            image={uploadedImage}
+            width={200}
+            height={200}
+            border={15}
+            color={[255, 255, 255, 0.6]}
+            scale={1.2}
+            rotate={0}
+          />
+          <div className="file-input-container">
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+              style={{ display: "none" }}
+              id="image-upload"
+            />
+            <label htmlFor="image-upload" className="upload-label">
+              Click here to upload your picture!
+            </label>
+          </div>
+        </div>
         <div>
           <div>Favorite Genres</div>
           <div>I am looking for:</div>
