9b83f52dc9d9c0ed97df952805b0580ecc285592
justmann <mj.justmann@gmail.com>
2024-01-10 12:23:51 -0600

    updated counts

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,11 +1,11 @@
 const calculateStatus = (count) => {
   if (count >= 50) {
     return "Book Lover";
-  } else if (count >= 25) {
+  } else if (count >= 30) {
     return "Scholar";
-  } else if (count >= 10) {
+  } else if (count >= 20) {
     return "Librarian";
-  } else if (count >= 5) {
+  } else if (count >= 10) {
     return "Book Collector";
   } else if (count >= 1) {
     return "Bookworm";
